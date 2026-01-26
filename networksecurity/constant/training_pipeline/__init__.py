9a3700c345db735c841277c2ec50270ea66a3eb3
Sriram14890 <sriram140606@gmail.com>
2026-01-26 18:11:13 +0530

    Data Transformation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -6,7 +6,7 @@ import numpy as np
 """
 Defining all the constant variables for training pipeline
 """
-TARGERT_COLUMN="Result"
+TARGET_COLUMN="Result"
 PIPELINE_NAME: str="NetworkSecurity"
 ARTIFACT_DIR: str="Artifacts"
 FILE_NAME: str="PhishingData.csv"
@@ -32,3 +32,20 @@ DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+## kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
